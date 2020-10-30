e1216b51f434359d3b09ebde49a1eec8bba7f6f2
yogi3915 <yogi_ferdian3915@yahoo.com>
2020-10-30 15:00:36 +0700

    oauth

--- client/index.js
+++ client/index.js
@@ -41,3 +41,35 @@ Your browser does not support the audio element.
         console.log(err);
     })
 }
+
+function onSignIn(googleUser) {
+
+    var google_access_token = googleUser.getAuthResponse().id_token;
+
+    console.log(google_access_token);
+
+    $.ajax({
+        method: "POST",
+        url: SERVER + `/users/googleLogin`,
+        data: {
+            google_access_token
+        }
+    })
+    .done(response => {
+
+        localStorage.setItem("access_token", response.access_token)
+        console.log(localStorage);
+
+    })
+    .fail(err => {
+        console.log("errrooooooororoo");
+    })
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
+  }
+
--- server/routes/tweet.js
+++ server/routes/tweet.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const TweetController = require("../controllers/tweet")
-const authentication = require("../middlewares/authentication")
-const authorization = require("../middlewares/authorization")
+const weatherAPIController = require("../controllers/weatherAPI")
+const newsAPIController = require("../controllers/newsAPI") 
+// const authentication = require("../middlewares/authentication")
+// const authorization = require("../middlewares/authorization")
 const router = express.Router()
 
-router.use(authentication)
+// router.use(authentication)
 
 //liat tweet si User
 router.get("/", TweetController.showAll)
@@ -13,10 +15,23 @@ router.get("/", TweetController.showAll)
 router.post("/", TweetController.create)
 
 //edit tweet si User
-router.put("/:id", authorization, TweetController.edit)
+// router.put("/:id", authorization, TweetController.edit)
 
 //delete tweet si User
-router.delete("/:id", authorization, TweetController.delete)
+// router.delete("/:id", authorization, TweetController.delete)
+
+
+
+// Weather APIs
+router.get('/country', weatherAPIController.weatherCountry)
+router.get('/state', weatherAPIController.stateinCountries)
+router.get('/cities', weatherAPIController.citiesInState)
+router.get('/city', weatherAPIController.cityData)
+
+// News APIs
+router.get('/topnews', newsAPIController.topHeadlinesNews)
+router.get('/everynews', newsAPIController.everythingNews)
+router.get('/source', newsAPIController.sourceNews)
 
 module.exports = router
 
